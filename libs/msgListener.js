c1e9e24d5e108b9d4cd5fec20b1893c0a60a57ad
XNS-ivy <ayramusic.nightcore@gmail.com>
2024-04-21 11:11:02 +0700

    add listener from chat

---
+++ libs/msgListener.js
@@ -0,0 +1,5 @@
+async function Listener(theMessage) {
+  console.log(theMessage);
+}
+
+module.exports = { Listener };
--- libs/wasock.js
+++ libs/wasock.js
@@ -3,23 +3,29 @@ const {
   useMultiFileAuthState,
 } = require("@whiskeysockets/baileys");
 const pino = require("pino");
+const { Listener } = require("./msgListener.js");
 
 async function kotoriSock() {
   const auth = await useMultiFileAuthState("session");
-  const kotori = makeWASocket({ 
+  const kotori = makeWASocket({
     printQRInTerminal: true,
-    browser: ["itsuka-kotori","Firefox","1.0.0"],
+    browser: ["itsuka-kotori", "Firefox", "1.0.0"],
     auth: auth.state,
-    logger: pino({level: "silent"}),
-   });
-   kotori.ev.on("creds.update",auth.saveCreds);
-   kotori.ev.on("connection.update", ({connection}) =>{
+    logger: pino({ level: "silent" }),
+  });
+  kotori.ev.on("creds.update", auth.saveCreds);
+  kotori.ev.on("connection.update", ({ connection }) => {
     if (connection === "open") {
       console.log("is open");
-    } else if(connection === "close"){
+    } else if (connection === "close") {
       kotoriSock();
     }
-   })
+  });
+  kotori.ev.on("messages.upsert", async (m) => {
+    if (!m.messages) return;
+    const theMessage = m.messages[0];
+    await Listener(theMessage);
+  });
 }
 
 module.exports = { kotoriSock };
